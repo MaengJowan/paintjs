c9c829396a8d56a30820d73582bce7478203d979
MaengJowan <qleksqnddj33@naver.com>
2019-05-05 22:50:34 +0900

    ChangeColor Update

--- app.js
+++ app.js
@@ -1,27 +1,46 @@
 const canvas = document.querySelector("#jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+
+Array.from(colors).forEach(function color(color){
+    color.addEventListener("click", handleChangeColor)
+});
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle ="#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
 function stopPainting(){
     painting = false;
 }
-
+function startPainting(){
+    painting = true;
+}
 function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
 
+    //만약 painting이 true이면
+    if(painting){
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    } else{
+        ctx.moveTo(x, y);
+        ctx.beginPath();
+    }
 }
-function onMouseDown(event){
-    console.log(event);
-    stopPainting();
-
-}
-function onMouseUp(event){
-    painting = true;
+function handleChangeColor(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
 }
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup" , onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup" , stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
